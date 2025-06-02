f0e9783d25605e33907f74fff3da536724835551
edielson <edielsongomes2712@gmail.com>
2025-06-02 20:53:23 -0300

    Commit das configurações H2, Swager e JPA

--- src/main/java/com/senai/projetointegrador/mecanicaespecializadabackend/MecanicaEspecializadaBackEndApplication.java
+++ src/main/java/com/senai/projetointegrador/mecanicaespecializadabackend/MecanicaEspecializadaBackEndApplication.java
@@ -1,7 +1,12 @@
 package com.senai.projetointegrador.mecanicaespecializadabackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "${info.build.name}", version = "${info.build.version}", description = "${info.app.description}",
+        contact = @io.swagger.v3.oas.annotations.info.Contact(name = "Time Arquitetura e APIs", email = "")))
+
 
 @SpringBootApplication
 public class MecanicaEspecializadaBackEndApplication {
