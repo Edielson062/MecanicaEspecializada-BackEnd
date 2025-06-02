122c6a38b900a93d6e885a361441c9199db860fd
edielson <edielsongomes2712@gmail.com>
2025-06-02 20:38:59 -0300

    Primeiro commit

---
+++ src/main/java/com/senai/projetointegrador/mecanicaespecializadabackend/MecanicaEspecializadaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.senai.projetointegrador.mecanicaespecializadabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecanicaEspecializadaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MecanicaEspecializadaBackEndApplication.class, args);
+    }
+
+}
