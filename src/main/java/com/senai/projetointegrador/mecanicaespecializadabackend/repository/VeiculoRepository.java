05e1b4604a96f449332dfd1c34a72845f6806238
unknown <alexandre.sc115@gmai.com>
2025-06-03 19:30:35 -0300

    Veiculo model/repository/service/controller

---
+++ src/main/java/com/senai/projetointegrador/mecanicaespecializadabackend/repository/VeiculoRepository.java
@@ -0,0 +1,8 @@
+package com.senai.projetointegrador.mecanicaespecializadabackend.repository;
+
+import com.senai.projetointegrador.mecanicaespecializadabackend.models.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VeiculoRepository extends JpaRepository<Veiculo,Integer> {
+
+}
